81c9120ea1c5495c13e0b6e3e5d7fefe5b3f545a
Davies Ashley <ashdavies@users.noreply.github.com>
2023-01-18 23:28:49 +0100

    Include service account for mastodon configuration

--- main.tf
+++ main.tf
@@ -1,6 +1,21 @@
+resource "google_service_account" "main" {
+  display_name = "Mastodon service account"
+  account_id   = "mastodon"
+}
+
 resource "google_storage_bucket" "main" {
   location                    = var.google_project_region
   name                        = var.google_storage_bucket
   public_access_prevention    = "enforced"
   uniform_bucket_level_access = true
+}
+
+resource "google_storage_bucket_iam_binding" "main" {
+  members = ["serviceAccount:${google_service_account.main.email}"]
+  bucket  = google_storage_bucket.main.name
+  role    = "roles/storage.admin"
+}
+
+resource "google_storage_hmac_key" "mastodon" {
+  service_account_email = google_service_account.main.email
 }
