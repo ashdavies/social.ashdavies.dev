9baa992185ca39f57582696ceaf41e6d4e2e1748
Davies Ashley <ashdavies@users.noreply.github.com>
2023-01-19 11:18:04 +0100

    Update configuration for current Google Cloud state

--- main.tf
+++ main.tf
@@ -1,21 +1,24 @@
-resource "google_service_account" "main" {
+resource "google_service_account" "default" {
   display_name = "Mastodon service account"
-  account_id   = "mastodon"
+  account_id   = "mastodon-social"
+  # account_id   = "mastodon"
 }
 
-resource "google_storage_bucket" "main" {
-  location                    = var.google_project_region
-  name                        = var.google_storage_bucket
-  public_access_prevention    = "enforced"
+resource "google_storage_bucket" "default" {
+  # location                    = var.google_project_region
+  # name                        = var.google_storage_bucket
+  # public_access_prevention    = "enforced"
+  name                        = "files-social-ashdavies-dev"
   uniform_bucket_level_access = true
+  location                    = "EU"
 }
 
-resource "google_storage_bucket_iam_binding" "main" {
-  members = ["serviceAccount:${google_service_account.main.email}"]
-  bucket  = google_storage_bucket.main.name
+resource "google_storage_bucket_iam_binding" "default" {
+  members = ["serviceAccount:${google_service_account.default.email}"]
+  bucket  = google_storage_bucket.default.name
   role    = "roles/storage.admin"
 }
 
-resource "google_storage_hmac_key" "mastodon" {
-  service_account_email = google_service_account.main.email
+resource "google_storage_hmac_key" "default" {
+  service_account_email = google_service_account.default.email
 }
